1e499eb419915dca26043e7af8d125ae46f76cf0
abir <abir2526@gmail.com>
2024-03-14 15:42:14 -0700

    Commit Message

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:bangla_audio_book/home_page.dart';
 import 'package:flutter/material.dart';
 
-import 'audio_paly_screen/player_screen.dart';
 
 void main() {
   runApp(const MyApp());
